25aadd4f63dabfb3b3799041765227c62cb37ec5
Isaac Akinsanya <IsaacAki247@gmail.com>
2026-04-27 19:05:52 +0100

    Added structure to the static components
    created the layout that I will be using

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-// import { useState } from 'react'
-import './App.css'
+import './styles/App.css'
+import Header from './components/Header';
+import CardsContainer from './components/CardsContainer';
 
 function App() {
   return(
-    <h1>Memory Card</h1>
+    <div className="app" style={{ border: '2px solid blue', padding: '10px'}}>
+      <div className="header" style={{ 
+        border: '2px solid red',
+        marginBottom: '10px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px'
+      }}>
+        <Header />
+      </div>
+      <div className="cards-container" style={{
+        border: '2px solid red',
+        padding: '10px',
+        display: 'grid',
+        gridTemplateRows: 'repeat(2, 1fr)',
+        gridTemplateColumns: 'repeat(6, 1fr)',
+        gap: '7px'
+      }}>
+        <CardsContainer />
+      </div>
+    </div>
   )
 }
 
